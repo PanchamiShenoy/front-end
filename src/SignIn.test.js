b88705ab811064a79d225ed4786e6aee92dd69f5
sjoshi36@uncc.edu <sjoshi36@uncc.edu>
2023-12-08 14:08:34 -0500

    e2e test added

--- src/SignIn.test.js
+++ src/SignIn.test.js
@@ -1,61 +1,31 @@
 import React from 'react';
-import { render, fireEvent, waitFor, screen } from '@testing-library/react';
-import axios from 'axios'; // Mock Axios
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import SignInPage from './SignInPage';
+import axios from 'axios';
 
-//jest.mock('axios'); // Mocking Axios
+jest.mock('axios');
 
-describe('SignInPage component', () => {
-  it('submits form and signs in successfully', async () => {
-    const mockedToken = 'mockedToken123'; // A mocked token for testing purposes
+describe('SignInPage Component', () => {
+  test('Submits the form with valid data', async () => {
+    const { getByLabelText, getByRole } = render(<SignInPage />);
+    const emailInput = getByLabelText('Enter your email');
+    const passwordInput = getByLabelText('Enter your password');
+    const submitButton = getByRole('button', { name: 'Sign In' });
 
-    // Mocking the axios post function to return a successful response
-    axios.post.mockResolvedValue({
-      data: {
-        token: mockedToken,
-      },
-    });
+  
+    axios.post.mockResolvedValueOnce({ data: { token: 'mockedToken' } });
 
-    render(<SignInPage />);
+    fireEvent.change(emailInput, { target: { value: 'user@example.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'password123' } });
 
-    // Simulate user input
-    fireEvent.change(screen.getByLabelText('Enter your email'), { target: { value: 'test@example.com' } });
-    fireEvent.change(screen.getByLabelText('Enter your password'), { target: { value: 'password123' } });
+    fireEvent.click(submitButton); // Submit the form
 
-    // Simulate form submission
-    fireEvent.click(screen.getByRole('Submit button'));
-
-    // Wait for the async operation (API call) to resolve
     await waitFor(() => {
-        expect(localStorage.setItem).toHaveBeenCalledWith('token', mockedToken);
-     // expect(axios.post).toHaveBeenCalledTimes(1); // Ensure axios.post was called once
-       // Check local storage for token
-      // Add additional assertions based on your application flow
-    });
-   // expect(localStorage.setItem).toHaveBeenCalledWith('token', mockedToken);
-  });
-
-  it('handles unsuccessful sign-in', async () => {
-    // Mocking axios.post to return an error response
-    axios.post.mockRejectedValue({
-      response: {
-        status: 400,
-      },
-    });
-
-    render(<SignInPage />);
-
-    // Simulate user input
-    fireEvent.change(screen.getByLabelText('Enter your email'), { target: { value: 'invalid@example.com' } });
-    fireEvent.change(screen.getByLabelText('Enter your password'), { target: { value: 'invalidpassword' } });
-
-    // Simulate form submission
-    fireEvent.click(screen.getByRole('Submit button'));
-
-    // Wait for the async operation (API call) to resolve
-    await waitFor(() => {
-      expect(axios.post).toHaveBeenCalledTimes(1); // Ensure axios.post was called once
-      // Add assertions for error handling based on your application flow
+      expect(axios.post).toHaveBeenCalledTimes(1);
+      expect(axios.post).toHaveBeenCalledWith('http://localhost:5000/signin', {
+        email: 'user@example.com',
+        password: 'password123',
+      });
     });
   });
 });
